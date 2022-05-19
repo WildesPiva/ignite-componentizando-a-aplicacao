568b704debac9f62b2c9757dc26a7b9e5056b6fc
WildesPiva <wildeshenrique8@gmail.com>
2022-05-18 21:52:45 -0300

    Finalizando componentização

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface SidebarProps {
+  activeGenreId: number;
+  onClickGenre: (id: number) => void;
+  genres: Genre[];
+}
+
+export function SideBar({ genres, onClickGenre, activeGenreId }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickGenre(genre.id)}
+            selected={activeGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
